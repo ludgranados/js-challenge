b1d5be98927bec3cf6b18384b82c4aa17f4cdae9
Ludwin Granados <ludwingranados@gmail.com>
2021-02-23 17:20:43 -0800

    first commit

---
+++ js/js-challenge.js
@@ -0,0 +1,32 @@
+/** Exercise 1 */
+
+// Initializing name in a string to a variable called fullName
+let fullName = " Ludwin Granados";
+
+// Initialize a welcome message string to a const variable
+const message = 'Welcome!';
+
+// Concatenate the two strings to form a new string
+// User the alert method to render the new string
+alert(message + fullName)
+
+
+/** Exercise 2 */
+
+// Assign the numbers 1-6 to six different variables
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var e = 5;
+var f = 6;
+
+// Using arithmetic operators do the following
+// Sum up all six values and assign to a new variable and console.log() the variable
+console.log(a + b + c + d + e + f);
+
+// Sum up the first three values and multiply the total by the 4th value,
+// then assign this to a new variable and console.log() the variable
+var g = a + b + c;
+let total = g * d;
+console.log(total)
